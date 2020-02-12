10692cd8a2a697c0337a1d32d2146a3f4b3b52bc
tnnmigga <tnnmigga@outlook.com>
2020-02-12 23:15:30 +0800

    显示图片

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Gallery(models.Model):
     """Model definition for Gallery."""
     description = models.CharField(default='在这里填作品简介',max_length=100)
-    image = models.ImageField(default='default.png', upload_to='image/', height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(default='default.png', upload_to='image/')
     title = models.CharField(default='作品标题', max_length=50)
 
     def __str__(self):
