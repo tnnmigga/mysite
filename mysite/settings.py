3d6c8c819e4ab668721d198ec7f3e7dc5be430cd
tnnmigga <tnnmigga@outlook.com>
2020-02-17 19:42:07 +0800

    更新

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery.apps.GalleryConfig'
+    'gallery',
+    'blog'
 ]
 
 MIDDLEWARE = [
